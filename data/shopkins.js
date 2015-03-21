4fd32f8c4d441aa1eb008bc313e1440b51e3a988
Eric Katz <ericpkatz@gmail.com>
2015-03-21 21:43:02 +0000

    simplify routes

--- data/shopkins.js
+++ data/shopkins.js
@@ -36,6 +36,291 @@
             "number": "2-006",
             "category": "fruit_and_veg",
             "value": "common"
+        },
+        {
+            "name": "Dippy Avocado",
+            "number": "2-007",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Silly Chilli",
+            "number": "2-008",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Chloe Flower",
+            "number": "2-009",
+            "category": "fruit_and_veg",
+            "value": "rare"
+        },
+        {
+            "name": "Sour Lemon",
+            "number": "2-010",
+            "category": "fruit_and_veg",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Juicy Orange",
+            "number": "2-011",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Corny Cob",
+            "number": "2-0012",
+            "category": "fruit_and_veg",
+            "value": "rare"
+        },
+        {
+            "name": "Garlic Rose",
+            "number": "2-013",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Boo-Hoo Onion",
+            "number": "2-014",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Dippy Avocado",
+            "number": "2-015",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Silly Chilli",
+            "number": "2-016",
+            "category": "fruit_and_veg",
+            "value": "common"
+        },
+        {
+            "name": "Toasty Pop",
+            "number": "2-017",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Brenda Blender",
+            "number": "2-018",
+            "category": "homewares",
+            "value": "rare"
+        },
+        {
+            "name": "Coffee Drip",
+            "number": "2-019",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Saucy Pan",
+            "number": "2-020",
+            "category": "homewares",
+            "value": "rare"
+        },
+        {
+            "name": "Ma Kettle",
+            "number": "2-021",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Zappy Microwave",
+            "number": "2-022",
+            "category": "homewares",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Lisa Litter",
+            "number": "2-023",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Lana Lamp",
+            "number": "2-024",
+            "category": "homewares",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Sizzles",
+            "number": "2-025",
+            "category": "homewares",
+            "value": "rare"
+        },
+        {
+            "name": "Toasty Pop",
+            "number": "2-026",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Brenda Blender",
+            "number": "2-027",
+            "category": "homewares",
+            "value": "rare"
+        },
+        {
+            "name": "Coffee Drip",
+            "number": "2-028",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Saucy Pan",
+            "number": "2-029",
+            "category": "homewares",
+            "value": "rare"
+        },
+        {
+            "name": "Ma Kettle",
+            "number": "2-030",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Zappy Microwave",
+            "number": "2-031",
+            "category": "homewares",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Lisa Litter",
+            "number": "2-032",
+            "category": "homewares",
+            "value": "common"
+        },
+        {
+            "name": "Lana Lamp",
+            "number": "2-033",
+            "category": "homewares",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Sizzles",
+            "number": "2-034",
+            "category": "homewares",
+            "value": "rare"
+        },
+        {
+            "name": "Slick Breadstick",
+            "number": "2-035",
+            "category": "bakery",
+            "value": "common"
+        },
+        {
+            "name": "Mary Muffin",
+            "number": "2-036",
+            "category": "bakery",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Carrie Carrot Cake",
+            "number": "2-037",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Mary Meringue",
+            "number": "2-038",
+            "category": "bakery",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Pecanna Pie",
+            "number": "2-039",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Choco Lava",
+            "number": "2-040",
+            "category": "bakery",
+            "value": "common"
+        },
+        {
+            "name": "Fifi Fruittart",
+            "number": "2-041",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Danny Danish",
+            "number": "2-042",
+            "category": "bakery",
+            "value": "common"
+        },
+        {
+            "name": "Cupcake Chic",
+            "number": "2-043",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Slick Breadstick",
+            "number": "2-044",
+            "category": "bakery",
+            "value": "common"
+        },
+        {
+            "name": "Mary Muffin",
+            "number": "2-045",
+            "category": "bakery",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Carrie Carrot Cake",
+            "number": "2-046",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Mary Meringue",
+            "number": "2-047",
+            "category": "bakery",
+            "value": "ultra_rare",
+            "glitz": true
+        },
+        {
+            "name": "Pecanna Pie",
+            "number": "2-048",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Choco Lava",
+            "number": "2-049",
+            "category": "bakery",
+            "value": "common"
+        },
+        {
+            "name": "Fifi Fruittart",
+            "number": "2-050",
+            "category": "bakery",
+            "value": "rare"
+        },
+        {
+            "name": "Danny Danish",
+            "number": "2-051",
+            "category": "bakery",
+            "value": "common"
+        },
+        {
+            "name": "Cupcake Chic",
+            "number": "2-052",
+            "category": "bakery",
+            "value": "rare"
         }
     ]
 }
--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 var express = require("express");
 var fs = require("fs");
+var _ = require("underscore");
+
+var categories = [
+  { 
+    name: "Fruit and Vegatables",
+    id: "fruit_and_veg"
+  },
+  { 
+    name: "Homewares",
+    id: "homewares"
+  },
+  { 
+    name: "Bakery",
+    id: "bakery"
+  },
+  { 
+    name: "Sweet Treats",
+    id: "sweet_treats"
+  }
+];
 
 var app = express();
 
 app.set("view engine", "jade");
 
+var renderView = function(title, category, req, res){
+  fs.readFile(__dirname + "/data/shopkins.js", function(err, data){
+    var shopkins = JSON.parse(data.toString()).shopkins;
+    shopkins = _.filter(shopkins, function(shopkin){
+      return shopkin.category == category;
+    });
+    res.render("list", {shopkins: shopkins, tab: category, title: title});  
+  });
+};
+
+app.use(function(req, res, next){
+ res.locals.categories = categories;
+ next();
+});
+
 app.get("/", function(req, res){
   res.render("index", {tab: "home", title: "Home"});  
 });
 
-app.get("/fruit_and_veg", function(req, res){
-  fs.readFile(__dirname + "/data/shopkins.js", function(err, data){
-    var shopkins = JSON.parse(data.toString()).shopkins;
-    res.render("fruit_and_veg", {shopkins: shopkins, tab: "fruit_and_veg", title: "Fruit and Vegetables"});  
+_.each(categories, function(category){
+  app.get("/" + category.id, function(req, res){
+    renderView(category.name, category.id, req, res);  
   });
 });
 
